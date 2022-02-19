867ce23cd83121e20eb142bec7e5454fe957424e
jiho kim <sokojiho2@gmail.com>
2022-02-19 12:56:20 +0900

    모의고사 문제

---
+++ src/com/programmers/VerTest.java
@@ -0,0 +1,14 @@
+package com.programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VerTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int[] a = {1,2,3,4,5};
+        int[] b = {2,1,2,3,2,4,2,5};
+        int[] c = {3,3,1,1,2,2,4,4,5,5};
+
+    }
+}
--- src/com/programmers/printer.java
+++ src/com/programmers/printer.java
@@ -3,30 +3,34 @@ import java.util.*;
 
 public class printer {
     public static void main(String[] args) {
+        int count=0;
+        int search = 0;
         int[] p = {2, 1, 3, 2};
         int location = 2;
-        int[] searchLocation = new int[100];
+        ArrayList<Integer> searchLocation = new ArrayList<>();
         Queue<Integer> que = new LinkedList<Integer>();
         for(int i =0; i<p.length;i++){ // 대기열을 큐에 저장
             que.add(p[i]);
         }
 
         for(int i=1; i<p.length;i++){ //자기자신을 빼고 검색하면 되기 때문 1부터시작
-            int search = que.poll();
+            if(que.isEmpty()){break;}
+            search = que.peek();
             if(search < p[i] || search == p[i]){  // search 할려는 값이 다음 값보다 작거나 같으면
+                que.poll(); //첫번째 값 뽑아서
                 que.add(search); // 큐 대기열 뒤에 저장
-
+                count++;
             }else{ //크다면 큐에 있는 값들을 다 빼서 배열에 저장.
-                for(int j =0;j<que.size();j++){
-                    searchLocation[i] = que.poll();
+                int qsize = que.size();
+                for(int j =0;j<qsize;j++){
+                    int temp = que.poll();
+                    searchLocation.add(temp);
+
 
                 }
             }
-            System.out.println(searchLocation[location]);
-
-
         }
-
+        System.out.println(searchLocation.size()-count+1);
 
     }
 }
