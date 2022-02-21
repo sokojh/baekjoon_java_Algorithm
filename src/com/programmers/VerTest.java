6dbc4b129cbe9a49149de241e286ec66a3ac856a
jiho kim <sokojiho2@gmail.com>
2022-02-21 17:55:40 +0900

    틀린거 수정

--- src/com/programmers/VerTest.java
+++ src/com/programmers/VerTest.java
@@ -5,10 +5,6 @@ import java.util.List;
 
 public class VerTest {
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        int[] a = {1,2,3,4,5};
-        int[] b = {2,1,2,3,2,4,2,5};
-        int[] c = {3,3,1,1,2,2,4,4,5,5};
 
     }
 }
